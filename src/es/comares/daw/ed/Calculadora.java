aa78bb1c223a3ff6305ee81005763e4c053ffeea
matilde <mjimbed098@iesmarquesdecomares>
2024-10-16 20:23:54 +0200

    Multiplicación

--- src/es/comares/daw/ed/Calculadora.java
+++ src/es/comares/daw/ed/Calculadora.java
@@ -5,6 +5,7 @@ public class Calculadora {
 	public static void main(String[] args) {
 		System.out.println("Calculadora");
 		System.out.println("Suma");
+		System.out.println("Multiplicación");
 	}
 
 }
